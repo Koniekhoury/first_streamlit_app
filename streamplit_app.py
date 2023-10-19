097810ce96345e71f6616e199b33e130f4d7f3ce
Koniekhoury <148462580+Koniekhoury@users.noreply.github.com>
2023-10-19 15:56:25 +0200

    Update streamplit_app.py
    typo

--- streamplit_app.py
+++ streamplit_app.py
@@ -5,6 +5,6 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🥣  Omega 3 & Blueberry oatmeal')
 streamlit.text('🥗  Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔  Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞  Avocado Toast')
+streamlit.text('🥑🍞  Avocado Toasts')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
