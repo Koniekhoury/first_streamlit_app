c41c21c935e424a96c8a402f82d60222d8f46019
Koniekhoury <148462580+Koniekhoury@users.noreply.github.com>
2023-10-23 11:29:19 +0200

    Update streamplit_app.py
    add a function

--- streamplit_app.py
+++ streamplit_app.py
@@ -39,7 +39,16 @@ except URLError as e:
   streamlit.error()
 
 
-# streamlit.stop()
+streamlit.header("The fruit load list contains:")
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cnx:
+    my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+    return my_cur.fetchall()
+
+if steamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
 
 # my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 # my_cur = my_cnx.cursor()
