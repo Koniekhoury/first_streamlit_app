316517862f5a8d041754094456bff3eaabc41c1b
Koniekhoury <148462580+Koniekhoury@users.noreply.github.com>
2023-10-23 11:51:06 +0200

    Update streamplit_app.py
    add button

--- streamplit_app.py
+++ streamplit_app.py
@@ -63,6 +63,18 @@ if streamlit.button('Add a Fruit to list'):
   streamlit.text(back_from_function)
 
 
+if streamlit.button('Get Fruit List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows= get_fuit_load_list()
+  my_cnx.close()
+  streamlit.dataframe(my_data_rows)
+
+
+
+
+
+
+
 
 
 
